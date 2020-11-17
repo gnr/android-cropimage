9b9fa7452b86490ef9d0119a143694fe960866f3
Gabriel Vanrenen <gabe@skimble.com>
2020-11-17 09:28:34 -0800

    updating to androidx and target 30

--- CropImageRepo/app/src/main/java/me/villani/lorenzo/cropimagerepo/MainActivity.java
+++ CropImageRepo/app/src/main/java/me/villani/lorenzo/cropimagerepo/MainActivity.java
@@ -1,7 +1,7 @@
 package me.villani.lorenzo.cropimagerepo;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import androidx.appcompat.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
